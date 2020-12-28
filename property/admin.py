aa16513e9f5764cb55977f78db91d23e23ae7918
Vladimir <v1ztep@gmail.com>
2020-12-28 16:28:43 +0300

    Добавил новую модель и выставил поиск квартиры через raw_id_fields

--- property/admin.py
+++ property/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Flat
+from .models import Flat, Complaint
 
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner',)
@@ -8,4 +8,8 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony',)
 
+class ComplaintAdmin(admin.ModelAdmin):
+    raw_id_fields = ('flat',)
+
 admin.site.register(Flat, FlatAdmin)
+admin.site.register(Complaint, ComplaintAdmin)
