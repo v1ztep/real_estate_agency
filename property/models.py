f37fc9860991fc096a421d3c3ec9a0b42141bdff
Vladimir <v1ztep@gmail.com>
2020-12-28 16:27:24 +0300

    Добавил модель Жалоба для пользователей

--- property/models.py
+++ property/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Flat(models.Model):
@@ -26,3 +27,11 @@ class Flat(models.Model):
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
+
+class Complaint(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Кто жаловался')
+    flat = models.ForeignKey(Flat, on_delete=models.CASCADE, verbose_name='Квартира, на которую пожаловались')
+    text = models.TextField(verbose_name='Текст жалобы')
+
+    def __str__(self):
+        return f'Пользователь "{self.user}", жалоба на квартиру: {self.flat}'
