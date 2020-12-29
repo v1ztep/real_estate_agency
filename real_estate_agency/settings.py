182b945afa4c0d39f65cd2a9dab8150c420c8950
Vladimir <v1ztep@gmail.com>
2020-12-29 21:52:55 +0300

    Добавил owner_pure_phone

--- real_estate_agency/settings.py
+++ real_estate_agency/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-
+    'phonenumber_field',
     'property.apps.PropertyConfig',
 ]
 
