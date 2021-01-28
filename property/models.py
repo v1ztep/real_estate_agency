ec71c9a7659fdf492120ed085aa3b29981092965
Vladimir <v1ztep@gmail.com>
2021-01-28 20:23:30 +0300

    Переименовал поле Flat.owner

--- property/models.py
+++ property/models.py
@@ -5,7 +5,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField("ФИО владельца", max_length=200)
+    owner_deprecated = models.CharField("ФИО владельца", max_length=200)
     owners_phonenumber = models.CharField("Номер владельца", max_length=20)
     owner_pure_phone = PhoneNumberField("Нормализованный номер владельца",null=True, blank=True)
     created_at = models.DateTimeField("Когда создано объявление", default=timezone.now, db_index=True)
