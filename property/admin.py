244c9df6208515f3ec678659d2dde33d999bffc1
Vladimir <v1ztep@gmail.com>
2021-01-28 21:18:26 +0300

    Выставил полю Owner M2M raw_id_fields

--- property/admin.py
+++ property/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ('town', 'address', 'owner',)
+    search_fields = ('town', 'address', 'owners',)
     readonly_fields = ["created_at",]
     list_display = ('address', 'price', 'new_building', 'construction_year', 'town',)
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony',)
-    raw_id_fields = ('liked_by',)
+    raw_id_fields = ('liked_by', 'owners',)
 
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('flat',)
