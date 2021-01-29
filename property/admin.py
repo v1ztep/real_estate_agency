710ef9d5cb134c1cd14d6fb30c357afdd97daa0f
Vladimir <v1ztep@gmail.com>
2021-01-29 20:43:25 +0300

    Добавил user raw_id_fields в Complaint, убрал owners из search_fields в Flat

--- property/admin.py
+++ property/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ('town', 'address', 'owners',)
+    search_fields = ('town', 'address',)
     readonly_fields = ["created_at",]
     list_display = ('address', 'price', 'new_building', 'construction_year', 'town',)
     list_editable = ('new_building',)
@@ -10,7 +10,7 @@ class FlatAdmin(admin.ModelAdmin):
     raw_id_fields = ('liked_by', 'owners',)
 
 class ComplaintAdmin(admin.ModelAdmin):
-    raw_id_fields = ('flat',)
+    raw_id_fields = ('flat','user')
 
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('owner_flats',)
