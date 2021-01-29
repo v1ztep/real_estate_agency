f0a16011861d0827331f0162341df2e055acbb0e
Vladimir <v1ztep@gmail.com>
2021-01-29 21:57:28 +0300

    Добавил функции назначения flat_to_owner и owner_to_flat

--- property/migrations/0015_auto_20210129_1717.py
+++ property/migrations/0015_auto_20210129_1717.py
@@ -3,15 +3,29 @@
 from django.db import migrations
 
 
-def set_flats_to_owners(apps, schema_editor):
+def set_flat_to_owner(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
     Owner = apps.get_model('property', 'Owner')
     for flat in Flat.objects.all():
-        Owner.objects.get_or_create(owner=flat.owner,
+        owner, created = Owner.objects.get_or_create(owner=flat.owner_deprecated,
                                     owner_pure_phone=flat.owner_pure_phone,
                                     defaults={
                                         'owners_phonenumber': flat.owners_phonenumber,
                                     })
+        if not created:
+            owner.owner_flats.set(flat)
+
+def set_owner_to_flat(apps, schema_editor):
+    Flat = apps.get_model('property', 'Flat')
+    Owner = apps.get_model('property', 'Owner')
+    for owner in Owner.objects.all():
+        flat, created = Flat.objects.get_or_create(owner_deprecated=owner.owner,
+                                    owner_pure_phone=owner.owner_pure_phone,
+                                    defaults={
+                                        'owners_phonenumber': owner.owners_phonenumber,
+                                    })
+        if not created:
+            flat.owners.set(owner)
 
 class Migration(migrations.Migration):
 
@@ -20,5 +34,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(set_flats_to_owners),
+        migrations.RunPython(set_flat_to_owner),
+        migrations.RunPython(set_owner_to_flat),
     ]
