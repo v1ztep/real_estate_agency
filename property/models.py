ddd224e7871d2827cdbc7f85d27ef65b183d14d4
Vladimir <v1ztep@gmail.com>
2021-02-04 20:57:04 +0300

    Отформатировал по PEP8

--- property/models.py
+++ property/models.py
@@ -8,12 +8,13 @@ class Flat(models.Model):
     owners = models.ManyToManyField('Owner', verbose_name='Собственники')
 
     created_at = models.DateTimeField('Когда создано объявление', default=timezone.now, db_index=True)
-    
+
     description = models.TextField('Текст объявления', blank=True)
     price = models.IntegerField('Цена квартиры', db_index=True)
 
     town = models.CharField('Город, где находится квартира', max_length=50, db_index=True)
-    town_district = models.CharField('Район города, где находится квартира', max_length=50, blank=True, help_text='Чертаново Южное')
+    town_district = models.CharField('Район города, где находится квартира', max_length=50, blank=True,
+                                     help_text='Чертаново Южное')
     address = models.TextField('Адрес квартиры', help_text='ул. Подольских курсантов д.5 кв.4')
     floor = models.CharField('Этаж', max_length=3, help_text='Первый этаж, последний этаж, пятый этаж')
 
@@ -24,12 +25,15 @@ class Flat(models.Model):
     active = models.BooleanField('Активно-ли объявление', db_index=True)
     construction_year = models.IntegerField('Год постройки здания', null=True, blank=True, db_index=True)
 
-    new_building = models.NullBooleanField('Новое здание', default=None, choices = [(None,'Не заполнено'),(True,'Новостройка'),(False,'Старое здание')])
+    new_building = models.NullBooleanField('Новое здание', default=None,
+                                           choices=[(None, 'Не заполнено'), (True, 'Новостройка'),
+                                                    (False, 'Старое здание')])
 
     liked_by = models.ManyToManyField(User, related_name='liked_flats', verbose_name='Кто лайкнул')
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
 
 class Complaint(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Кто жаловался')
@@ -38,6 +42,7 @@ class Complaint(models.Model):
 
     def __str__(self):
         return f'Пользователь "{self.user}", жалоба на квартиру: {self.flat}'
+
 
 class Owner(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200, db_index=True)
@@ -48,4 +53,3 @@ class Owner(models.Model):
 
     def __str__(self):
         return self.owner
-
