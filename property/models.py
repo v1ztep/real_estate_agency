d47f1e972b947bd4d1362a8a7bebcd5cdde47b97
Vladimir <v1ztep@gmail.com>
2021-02-05 00:18:54 +0300

    Разрешил полю liked_by оставаться пустым в админке

--- property/models.py
+++ property/models.py
@@ -41,8 +41,7 @@ class Flat(models.Model):
                                            db_index=True)
 
     liked_by = models.ManyToManyField(User, related_name='liked_flats',
-                                      verbose_name='Кто лайкнул', null=True,
-                                      blank=True)
+                                      verbose_name='Кто лайкнул', blank=True)
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
